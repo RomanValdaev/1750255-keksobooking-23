dfe43c9731f7f92d2bf9af7bb22c8935c6d10e03
Roman <rvaldaev@gmail.com>
2021-06-26 14:46:46 +0700

    Правда или действие (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 // Cоздаем точку входа
-import {createOffer} from './create-offer.js'; // импортировал функцию из модуля
-import {generateAddMarkup} from './generate-add-markup.js'; // импортировал функцию из модуля
+import {createOffer} from './create-offer.js';
+import {generateAddMarkup} from './generate-add-markup.js';
+import {formValidation} from './form-validation.js';
+// import {switchMap} from './switch-map.js';
 
 const NUMBER_OF_ARRAYS = 1; // создал константу в блоке, где она используется
-
-// создаем случайный массив данных
 const similarOffers = new Array(NUMBER_OF_ARRAYS).fill(null).map(() => createOffer());
-
 generateAddMarkup(similarOffers[0]);
+formValidation();
+
+// console.log(formValidation());
